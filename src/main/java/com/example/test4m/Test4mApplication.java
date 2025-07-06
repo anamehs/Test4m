e35abcbd795dbee3e803c22cd342be653c818673
BrokenL <sokolenkomihail2010@gmail.com>
2025-07-06 09:36:34 +0300

    Initial commit

---
+++ src/main/java/com/example/test4m/Test4mApplication.java
@@ -0,0 +1,13 @@
+package com.example.test4m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4mApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test4mApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test4m/Test4mApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test4m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4mApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
